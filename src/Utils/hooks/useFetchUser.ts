1c06b02bacb5ce7bc9918f1779b70319bb899f70
강민재 <minijae011030@naver.com>
2025-01-21 04:32:32 +0900

    Refactor: console 제거

--- src/Utils/hooks/useFetchUser.ts
+++ src/Utils/hooks/useFetchUser.ts
@@ -11,22 +11,18 @@ export function useFetchUser() {
   } = useQuery({
     queryKey: ["userInfo"],
     queryFn: async () => {
-      console.log("start query");
       const result = await getAccount();
 
       if (result.result) {
         return result.profileResult as UserInfoInterface;
       } else {
-        console.log("Error");
         throw new Error("Failed to fetch user information.");
       }
     },
-    // staleTime: 0,
     staleTime: 10 * 60 * 1000,
     cacheTime: 20 * 60 * 1000,
     onError: (error: Error) => {
       alert("사용자 정보를 불러오지 못했습니다.");
-      console.error(error);
     },
   });
 
