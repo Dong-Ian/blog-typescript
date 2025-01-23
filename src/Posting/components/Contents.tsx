38c73e92ad29f4e20bb0d0d71f84eb324d4788a9
강민재 <minijae011030@naver.com>
2025-01-24 05:08:27 +0900

    Feat: 에디터 사진 업로드 기능
    firebse와 연동

--- src/Posting/components/Contents.tsx
+++ src/Posting/components/Contents.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { TextFieldProps } from "Posting/types/Posting.type";
 import MDEditor from "@uiw/react-md-editor";
+import { TextFieldProps } from "Posting/types/Posting.type";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../Firebase";
+import Button from "Utils/components/Button";
 
 const Contents: React.FC<TextFieldProps> = ({ value, onChange }) => {
+  const imageHandler = () => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+
+    input.addEventListener("change", async () => {
+      const file = input.files ? input.files[0] : null;
+      if (!file) return;
+
+      const storageRef = ref(storage, `image/${Date.now()}`);
+      try {
+        await uploadBytes(storageRef, file).then((snapshot) => {
+          getDownloadURL(snapshot.ref).then((url) => {
+            const updatedValue = `${value || ""}\n![image](${url})`;
+            onChange(updatedValue);
+          });
+        });
+      } catch (error) {
+        alert("이미지 업로드 실패");
+      }
+    });
+  };
+
   const handleChange = (value?: string) => {
     if (value !== undefined) {
       onChange(value);
@@ -11,6 +38,9 @@ const Contents: React.FC<TextFieldProps> = ({ value, onChange }) => {
 
   return (
     <div style={{ marginBottom: "30px" }}>
+      <div style={{ marginBottom: "10px" }}>
+        <Button onClick={imageHandler} text="이미지 업로드" />
+      </div>
       <MDEditor value={value} onChange={handleChange} height={500} />
     </div>
   );
