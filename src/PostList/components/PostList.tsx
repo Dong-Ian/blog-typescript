8a3e6efb5a31dbfeb6d30371f722c6ee931919b3
강민재 <minijae011030@naver.com>
2025-02-15 19:08:29 +0900

    Refactor: 사용하지 않는 import 제거

--- src/PostList/components/PostList.tsx
+++ src/PostList/components/PostList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as cheerio from "cheerio";
 import styles from "../styles/postlist.module.css";
