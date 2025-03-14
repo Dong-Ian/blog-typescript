fbf969c1a3af5aade95a106618f935c6b2d19a0c
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-14 10:19:00 +0900

    Chore: tailwindcss 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import tailwindTypography from "@tailwindcss/typography";
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    screens: {
+      "sm-mobile": { max: "766px" }, // 작은 모바일 (766px 이하)
+      mobile: "767px", // 모바일 (767px 이상)
+      tablet: "990px", // 태블릿 (990px 이상)
+      laptop: "1200px", // 노트북 (1200px 이상)
+      desktop: "1600px", // 데스크탑 (1600px 이상)
+    },
+    extend: {
+      fontSize: {
+        sm: "var(--font-size)", // 기존 sm 값 유지
+        md: "calc(var(--font-size) + 4px)", // 중간 크기
+        lg: "calc(var(--font-size) + 8px)", // 큰 크기
+        xs: "calc(var(--font-size) - 4px)", // 가장 작은 사이즈 추가
+        xxs: "calc(var(--font-size) - 8px)", // 가장 작은 사이즈 추가
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        "mos-main-500": "#4ecdc4",
+        "mos-main-700": "#45b8b0",
+        "mos-green-500": "#348037",
+        "mos-green-300": "#C8E6C9",
+        "mos-green-100": "#E7F6EA",
+        "mos-gray-700": "#495057",
+        "mos-gray-500": "#6C757D",
+        "mos-gray-300": "#565656",
+        "mos-gray-100": "#D5D5D5",
+        "mos-white-gray-100": "#F8F9FA",
+        "mos-white-gray-300": "#DEE2E6",
+        "mos-blue-700": "#1E77D2",
+        "mos-blue-500": "#0D6EFD",
+        "mos-blue-300": "#BDDFFB",
+        "mos-blue-100": "#E3F3FD",
+        "mos-pink-500": "#D21EA8",
+        "mos-pink-300": "#EFA1DD",
+        "mos-pink-100": "#F0D2E9",
+        "mos-coral-500": "#FF6B6B",
+        "btn-border-main-color": "#e9ecef",
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate, tailwindTypography],
+} satisfies Config;
